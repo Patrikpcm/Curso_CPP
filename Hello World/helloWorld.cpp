cfd02a4daac50082f689a554c7e0896fe0839623
patrikpcm <patrikpcm@hotmail.com>
2023-04-11 17:52:51 -0300

    arquivo Hello World

--- Hello World/helloWorld.cpp
+++ Hello World/helloWorld.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+//using namespace std;
+/* Utilizando esse comando é possível importar todos os namespaces da biblioteca
+iostream, sem necessidade de declar o namespace antes da função utilizada da biblioteca
+como no exemplo abaixo*/
+
 int main(){
     std::cout << "Hello World" << std::endl;
     return 0;
