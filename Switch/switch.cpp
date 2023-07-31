bc61b0fdafbda3bc55d380e08eaffa4f69b82b2e
patrikpcm <patrikpcm@hotmail.com>
2023-07-31 17:39:16 -0300

    Código básico da função switch

---
+++ Switch/switch.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num=10;
+    switch (num){
+
+    case 10:
+        cout << "número = 10" << endl;
+        break;
+    
+    case 5:
+        cout << "número = 5" << endl;
+        break;
+    
+    case 0:
+        cout << "número = 0" << endl;
+        break;
+    
+    default:
+        break;
+    }
+}
