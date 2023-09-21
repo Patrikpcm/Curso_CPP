0c708f9aa747f9de02e6513e3a01ed58bb5e735e
patrikpcm <patrikpcm@hotmail.com>
2023-09-21 14:42:55 -0300

    Exemplo de uso da classe Stack em C++

---
+++ Classe Stack/stack.cpp
@@ -0,0 +1,30 @@
+/*
+Classe stack é uma classe que cria a estrutura de dados Pilha (LIFO)
+*/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+
+    stack<int> pilha;
+
+    pilha.push(10);
+    pilha.push(20);
+    pilha.push(30);
+    pilha.push(40);
+    //pilha: 10, 20, 30, 40
+
+    cout << "Tamanho da pilha: " << pilha.size() << endl;
+
+    while(!pilha.empty()){
+        int e = pilha.top();
+        cout << e << " ";
+        pilha.pop();
+    }
+    cout << endl;
+
+    
+return 0;
+}
